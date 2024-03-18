f8dcf22af72da15655f1a46818502bae3bf1f851
Seven <phoenixpyra7@gmail.com>
2024-03-18 17:14:25 -0500

    I removed the About as I wanted to take this in a different direction and decided I wanted to bring it back, added back to nav tabs, pages and main  jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom/client'
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap.min.css'
 
 import App from './App';
 import Error from './pages/Error';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
 import About from './pages/About';
+import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 
 // Define the accessible routes, and which components respond to which URL
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,10 @@
 export default function About() {
-  return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-        The Phoenix.
-      </p>
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>
+          How many pages do I have to write stories on?.
+        </p>
+      </div>
+    );
+  }
